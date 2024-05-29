eeba6e4580c6c3be3de97c3c483b3add71381c70
Joanne <joanne.bazin@gmail.com>
2024-05-29 17:22:34 +0200

    feat: add categories filter

--- components/CategorieList.tsx
+++ components/CategorieList.tsx
@@ -3,9 +3,37 @@ import AddCategory from "./actions/forms/AddCategory";
 import { Card, CardTitle } from "./ui/card";
 import { useFetchCategories } from "./hooks/useFetchCategories";
 import { Loader } from "./layout/Loader";
+import { CategoryProps, Item } from "@/lib/types";
+import { useState } from "react";
 
 export default function CategorieList() {
   const { categories, loading, error, refetch } = useFetchCategories();
+  const [selection, setSelection] = useState<{
+    Films: Item[];
+    Series: Item[];
+  } | null>();
+
+  const handleSelection = async (category: CategoryProps) => {
+    console.log(category);
+
+    try {
+      const response = await fetch(`/api/category/${category.id}`, {
+        method: "GET",
+        headers: { "Content-type": "application/json" },
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to delete");
+      }
+
+      const result = await response.json();
+
+      setSelection(result);
+      console.log(selection);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div>
@@ -16,11 +44,31 @@ export default function CategorieList() {
             <Loader />
           ) : (
             categories.map((category) => (
-              <Card className="p-4" key={category.id}>
+              <Card
+                className="p-4 cursor-pointer"
+                onClick={() => handleSelection(category)}
+                key={category.id}
+              >
                 <CardTitle>{category.name}</CardTitle>
               </Card>
             ))
           )}
+        </div>
+        <div>
+          <ul>
+            {selection
+              ? selection.Films.map((film, index) => (
+                  <li key={index}>{film.title}</li>
+                ))
+              : null}
+          </ul>
+          <ul>
+            {selection
+              ? selection.Series.map((serie, index) => (
+                  <li key={index}>{serie.title}</li>
+                ))
+              : null}
+          </ul>
         </div>
       </div>
     </div>
