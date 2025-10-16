186d556d9f01f2254e4b40704cf550eeecd2b9bf
Joanne <joanne.bazin@gmail.com>
2025-10-16 15:31:26 +0200

    style: add responsive design into public pages

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import AuthForm from "@/src/features/auth/components/AuthForm";
 import { useSession } from "@/src/lib/auth-client";
+import { MoveLeft } from "lucide-react";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
@@ -16,14 +18,28 @@ const Authentification = () => {
   }, [router, session, isPending]);
 
   return (
-    <main className="flex flex-col gap-8 items-center my-12">
-      <h1 className="text-2xl font-semibold">
+    <main className="max-w-[500px]">
+      <h1 className="text-xl sm:text-2xl font-semibold my-12 text-center">
         {isLogin ? "Connexion" : "Inscription"}
       </h1>
-      <AuthForm isLogin={isLogin} />
-      <button onClick={() => setIsLogin(!isLogin)}>
-        {isLogin ? "Créer un compte" : "Déjà un compte ?"}
-      </button>
+
+      <div className="flex flex-col gap-6 sm:gap-8 items-center">
+        <AuthForm isLogin={isLogin} />
+        <button
+          onClick={() => setIsLogin(!isLogin)}
+          className="text-sm sm:text-base text-foreground underline hover:text-accent"
+        >
+          {isLogin ? "Créer un compte" : "Déjà un compte ?"}
+        </button>
+        <div className="w-full h-[2px] bg-gradient-to-r from-transparent via-accent to-transparent my-4"></div>
+      </div>
+      <Link
+        href="/"
+        className="flex justify-center items-center gap-3 text-sm my-4"
+      >
+        <MoveLeft size={28} />
+        Retour
+      </Link>
     </main>
   );
 };
