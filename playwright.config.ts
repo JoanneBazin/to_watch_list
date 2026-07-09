c3291df83340444090f64c1e491b2e9ae2f7e027
Joanne <joanne.bazin@gmail.com>
2026-07-09 17:36:13 +0200

    test(e2e): add global-teardown to clean db and update tests local run script

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,48 +1,25 @@
 import { defineConfig, devices } from "@playwright/test";
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
-
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
-
 const isCI = !!process.env.CI;
 
 export default defineConfig({
   testDir: "./tests/e2e",
-  /* Run tests in files in parallel */
+  globalTeardown: "./tests/e2e/playwright.global-teardown.ts",
   fullyParallel: false,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 0 : 0,
-  /* Opt out of parallel tests on CI. */
+  retries: process.env.CI ? 2 : 0,
   workers: 1,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   timeout: 180000,
   expect: {
     timeout: 60000,
   },
   use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: "http://localhost:3000",
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
-
     actionTimeout: 60000,
     navigationTimeout: 90000,
   },
-
-  /* Configure projects for major browsers */
   projects: [
     {
       name: "chromium",
@@ -80,7 +57,6 @@ export default defineConfig({
     // },
   ],
 
-  /* Run your local dev server before starting the tests */
   webServer: {
     command: isCI ? "npm run start" : "npm run dev",
     url: "http://localhost:3000",
